ede535fb7537b0c477b6157c12fdb50290f11810
masiwangs <ichwan@sociabuzz.com>
2022-05-16 17:19:51 +0000

    change project cost from apex to chartjs

--- components/dashboard/ProjectCost.vue
+++ components/dashboard/ProjectCost.vue
@@ -1,65 +1,39 @@
 <template>
-  <apexchart type="bar" width="100%" height="350" :options="chartOptions" :series="chartSeries"></apexchart>
+  <Bar
+    :chart-options="chartOptions"
+    :chart-data="chartData"
+  />
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        chartSeries: [{
-          name: 'Material',
-          data: [120, 110, 100, 90, 90, 90, ]
-        }, {
-          name: 'Jasa',
-          data: [70, 65, 60, 60, 50, 50, ]
-        }, {
-          name: 'Total',
-          data: [190, 175, 160, 150, 140, 140, ]
-        }],
-        chartOptions: {
-          chart: {
-            type: 'bar',
-            width: '100%'
-          },
-          plotOptions: {
-            bar: {
-              horizontal: false,
-              columnWidth: '55%',
-              endingShape: 'rounded'
-            },
-          },
-          dataLabels: {
-            enabled: false
-          },
-          stroke: {
-            show: true,
-            width: 2,
-            colors: ['transparent']
-          },
-          xaxis: {
-            categories: ['PRK', 'SKKI', 'Pengadaan', 'Kontrak', 'Pelaksanaan', 'Pembayaran'],
-          },
-          yaxis: {
-            title: {
-              text: 'Rp (juta)'
-            }
-          },
-          fill: {
-            opacity: 1
-          },
-          tooltip: {
-            y: {
-              formatter: function (val) {
-                return "Rp " + val + " juta"
-              }
-            }
-          }
-        },
-      }
+import { Bar } from 'vue-chartjs/legacy'
+
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+} from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+  name: 'BarChart',
+  components: {
+    Bar
+  },
+  props: {
+    chartData: {
+      type: Object,
+      default:() => {}
+    },
+    chartOptions: {
+      type: Object,
+      default:() => {}
     }
-  }
+  },
+}
 </script>
-
-<style>
-
-</style>
